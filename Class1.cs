2d2d9c9854902fe5ebe6d30011971d7492bfee4f
tvblackman1 <vbif.misha2012@yandex.ru>
2020-05-02 19:59:38 +0300

    Added code

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoutubeTop
+{
+    class Class1
+    {
+        static void Main()
+        {
+            MyForms.Show("first");
+            MyForms.Show("second");
+        }
+    }
+}
---
+++ MyForms.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace YoutubeTop
+{
+    class MyForms:Form
+    {
+        private Color baseColor;
+
+        public MyForms(string txt):base()
+        {
+            Text = txt;
+            Height = 200;
+            Width = 300;
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            baseColor = BackColor;
+            MouseEnter += (a, b) =>
+            {
+                BackColor = Color.Red;
+            };
+
+            MouseLeave += (a, b) =>
+            {
+                BackColor = baseColor;
+            };
+        }
+
+        public static void Show(string txt)
+        {
+            Application.Run(new MyForms(txt));
+        }
+    }
+}
